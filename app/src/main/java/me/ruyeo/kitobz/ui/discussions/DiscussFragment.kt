120d3576bd633d1ac8676e02d8f9585e30ec903e
rustamsobirov <carwolt123@gmail.com>
2022-07-15 14:24:43 +0500

    add style

--- app/src/main/java/me/ruyeo/kitobz/ui/discussions/DiscussFragment.kt
+++ app/src/main/java/me/ruyeo/kitobz/ui/discussions/DiscussFragment.kt
@@ -7,12 +7,31 @@ import android.view.View
 import android.view.ViewGroup
 import dagger.hilt.android.AndroidEntryPoint
 import me.ruyeo.kitobz.R
+import me.ruyeo.kitobz.adapter.DiscussAdapter
+import me.ruyeo.kitobz.databinding.FragmentDiscussBinding
 import me.ruyeo.kitobz.ui.BaseFragment
+import viewBinding
 
 @AndroidEntryPoint
 class DiscussFragment : BaseFragment(R.layout.fragment_discuss) {
 
+    private val binding by viewBinding { FragmentDiscussBinding.bind(it) }
+    private val adapter by lazy { DiscussAdapter() }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupUI()
+        setupObservers()
+    }
+
+    private fun setupUI() {
+        binding.apply {
+            rvDiscuss.adapter = adapter
+        }
+    }
+
+    private fun setupObservers() {
+
     }
 }
