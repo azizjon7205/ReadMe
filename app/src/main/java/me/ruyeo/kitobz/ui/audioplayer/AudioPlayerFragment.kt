b9c5badd3624e248884c722f8b5451bd6ff4099c
SadullayevBotir <91020070+SadullayevBotir@users.noreply.github.com>
2022-07-16 17:32:25 +0500

    \\

---
+++ app/src/main/java/me/ruyeo/kitobz/ui/audioplayer/AudioPlayerFragment.kt
@@ -0,0 +1,19 @@
+package me.ruyeo.kitobz.ui.audioplayer
+
+import android.os.Bundle
+import android.view.View
+import dagger.hilt.android.AndroidEntryPoint
+import me.ruyeo.kitobz.R
+import me.ruyeo.kitobz.ui.BaseFragment
+
+@AndroidEntryPoint
+class AudioPlayerFragment : BaseFragment(R.layout.fragment_audio_player) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
