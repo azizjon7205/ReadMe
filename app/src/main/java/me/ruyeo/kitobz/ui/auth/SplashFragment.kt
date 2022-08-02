cecc7de68bd20659a24f55c9e2b7b4e2a2a31394
SadullayevBotir <91020070+SadullayevBotir@users.noreply.github.com>
2022-08-02 12:08:40 +0500

    ./

--- app/src/main/java/me/ruyeo/kitobz/ui/auth/SplashFragment.kt
+++ app/src/main/java/me/ruyeo/kitobz/ui/auth/SplashFragment.kt
@@ -69,7 +69,7 @@ class SplashFragment : BaseFragment(R.layout.fragment_splash) {
             }
 
             override fun onFinish() {
-                findNavController().navigate(R.id.action_splashFragment_to_audioPlayerFragment)
+                findNavController().navigate(R.id.action_splashFragment_to_introFragment)
             }
         }
         timer!!.start()
