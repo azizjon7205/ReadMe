206d23d0ba9bc1d9002b0a37e2d52984df4a32c1
User <abbosmardiev1999@gmail.com>
2023-05-12 10:45:32 +0500

    add chucker

--- app/src/main/java/me/ruyeo/kitobz/di/ServerModule.kt
+++ app/src/main/java/me/ruyeo/kitobz/di/ServerModule.kt
@@ -1,11 +1,16 @@
 package me.ruyeo.kitobz.di
 
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import me.ruyeo.kitobz.data.remote.ApiService
 import me.ruyeo.kitobz.utils.Constants.BASE_URL
@@ -14,6 +19,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -23,10 +29,10 @@ class ServerModule {
 
     @Provides
     @Singleton
-    fun getRetrofit(client: OkHttpClient): Retrofit = Retrofit.Builder()
+    fun getRetrofit(factory: MoshiConverterFactory, client: OkHttpClient): Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .client(client)
-        .addConverterFactory(GsonConverterFactory.create())
+        .addConverterFactory(factory)
         .build()
 
     @Provides
@@ -34,7 +40,7 @@ class ServerModule {
     fun getApiService(retrofit: Retrofit): ApiService =
         retrofit.create(ApiService::class.java)
 
-    @Provides
+    /*@Provides
     @Singleton
     fun getClient(): OkHttpClient = OkHttpClient.Builder()
         .connectTimeout( 60, TimeUnit.SECONDS)
@@ -45,12 +51,53 @@ class ServerModule {
         })
         .addInterceptor(Interceptor { chain ->
             val builder = chain.request().newBuilder()
-             /* if (sharedPref.token != ""){
+             *//* if (sharedPref.token != ""){
                   builder.header("Authorization", "Bearer ${sharedPref.token}")
-              }*/
+              }*//*
             chain.proceed(builder.build())
         })
-        .build()
+        .build()*/
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMoshiConverterFactory(moshi: Moshi): MoshiConverterFactory {
+        return MoshiConverterFactory.create(moshi)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        beetoInterceptor: Interceptor,
+        chuckerInterceptor: ChuckerInterceptor,
+    ): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(beetoInterceptor)
+            .addInterceptor(chuckerInterceptor).build()
+    }
+
+    @Provides
+    fun provideBeetoInterceptor(): Interceptor {
+        return Interceptor {
+            val request1 = it.request()
+            val request = request1.newBuilder()
+//                .addHeader(AUTHORIZATION, "${userPreferenceManager.token}")
+//                .addHeader(HttpHeaders.ACCEPT_LANGUAGE, "uz")
+
+            val response = it.proceed(request.build())
+            response
+        }
+    }
+
+    @Provides
+    fun provideChuckerInterceptor(@ApplicationContext context: Context): ChuckerInterceptor {
+        return ChuckerInterceptor.Builder(context).build()
+    }
+
 
     @Singleton
     @Provides
